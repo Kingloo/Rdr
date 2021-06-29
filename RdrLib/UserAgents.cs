3cbd3af925eeaa12cde145cdfb0678e8a7312b42
Kingloo <k1ngloo@hotmail.com>
2021-06-29 17:32:13 +0200

    Fix UserAgent

--- RdrLib/UserAgents.cs
+++ RdrLib/UserAgents.cs
@@ -4,7 +4,7 @@ namespace RdrLib.Common
 {
 	public static class UserAgents
 	{
-		public const string Firefox_89_Windows = "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0";
+		public const string Firefox_89_Windows = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0";
 		public const string Edge_91_Windows = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36 Edg/91.0.864.59";
 		public const string Safari_13_1_MacOSX = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15";
 		public const string Chrome_85_Windows = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36";
