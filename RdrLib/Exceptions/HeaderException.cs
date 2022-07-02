728c871c938b421a48903631645cb32cac39c8cd
Kingloo <k1ngloo@hotmail.com>
2022-07-02 16:45:31 +0200

    Add Exceptions folder to RdrLib, and HeaderException

---
+++ RdrLib/Exceptions/HeaderException.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Runtime.Serialization;
+
+namespace RdrLib.Exceptions
+{
+	public class HeaderException : Exception
+	{
+		public string UnaddableHeader { get; init; } = string.Empty;
+
+        public HeaderException()
+		{ }
+
+		public HeaderException(string? message)
+            : base(message)
+		{ }
+
+		public HeaderException(string? message, Exception? innerException)
+            : base(message, innerException)
+		{ }
+
+		protected HeaderException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+		{ }
+	}
+}
