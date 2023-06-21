3e368502ac25ed7c27bc97c02d039aa061a9d689
Kingloo <k1ngloo@hotmail.com>
2023-06-21 21:00:25 +0200

    Add publish script

---
+++ publish-desktop-x64.ps1
@@ -0,0 +1,3 @@
+dotnet restore
+dotnet build .\Rdr.sln -c Release --no-restore --nologo
+dotnet publish .\Rdr\Rdr.csproj -c Release -r win10-x64 /p:PublishSingleFile=true --no-self-contained --no-restore
