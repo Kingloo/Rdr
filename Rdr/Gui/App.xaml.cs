1641181d1771657eb30f59ef2318ffb32d56fbc6
Kingloo <k1ngloo@hotmail.com>
2023-12-03 19:00:23 +0100

    Build a GenericHost in App.xaml.cs

--- Rdr/Gui/App.xaml.cs
+++ Rdr/Gui/App.xaml.cs
@@ -1,48 +1,140 @@
 using System;
-using System.IO;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Threading;
-using Rdr.Common;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using FileLogger;
+using RdrLib;
 
 namespace Rdr.Gui
 {
 	public partial class App : Application
 	{
-		private readonly string defaultDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-
-#if DEBUG
-		private const string defaultFileName = "RdrFeeds-test.txt";
-#else
-        private const string defaultFileName = "RdrFeeds.txt";
-#endif
+		private readonly IHost host;
+		private readonly ILogger<App> logger;
 
 		public App()
 		{
 			InitializeComponent();
+
+			host = BuildHost();
+
+			logger = host.Services.GetRequiredService<ILogger<App>>();
+		}
+
+		private static IHost BuildHost()
+		{
+			return new HostBuilder()
+				.ConfigureHostConfiguration(ConfigureHostConfiguration)
+				.ConfigureLogging(ConfigureLogging)
+				.ConfigureHostOptions(ConfigureHostOptions)
+				.ConfigureAppConfiguration(ConfigureAppConfiguration)
+				.ConfigureServices(ConfigureServices)
+				.Build();
+		}
+
+		private static void ConfigureHostConfiguration(IConfigurationBuilder builder)
+		{
+			builder
+				.AddCommandLine(Environment.GetCommandLineArgs())
+				.AddEnvironmentVariables();
+		}
+
+		private static void ConfigureLogging(HostBuilderContext context, ILoggingBuilder builder)
+		{
+			builder.AddConfiguration(context.Configuration.GetSection("Logging"));
+
+			builder.AddFileLogger();
+
+			builder.AddEventLog();
+
+			if (context.HostingEnvironment.IsDevelopment())
+			{
+				builder.AddDebug();
+			}
+		}
+
+		private static void ConfigureHostOptions(HostBuilderContext context, HostOptions options)
+		{
+			options.BackgroundServiceExceptionBehavior = BackgroundServiceExceptionBehavior.StopHost;
+			options.ShutdownTimeout = TimeSpan.FromSeconds(5d);
+		}
+
+		private static void ConfigureAppConfiguration(HostBuilderContext context, IConfigurationBuilder builder)
+		{
+			if (!context.HostingEnvironment.IsProduction())
+			{
+				string environmentMessage = $"environment is {context.HostingEnvironment.EnvironmentName}";
+
+				Console.Out.WriteLine(environmentMessage);
+				Console.Error.WriteLine(environmentMessage);
+				Debug.WriteLineIf(Debugger.IsAttached, environmentMessage);
+			}
+
+			builder.SetBasePath(AppContext.BaseDirectory);
+
+			const string permanent = "appsettings.json";
+			string environment = $"appsettings.{context.HostingEnvironment.EnvironmentName}.json";
+
+			builder
+				.AddJsonFile(permanent, optional: false, reloadOnChange: true)
+				.AddJsonFile(environment, optional: true, reloadOnChange: true);
+		}
+
+		private static void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+		{
+			services.AddSingleton<IFileLoggerSink, FileLoggerSink>();
+
+			services.Configure<RdrOptions>(context.Configuration.GetRequiredSection("RdrOptions"));
+
+			services.AddTransient<IRdrService, RdrService>();
+			services.AddTransient<IMainWindowViewModel, MainWindowViewModel>();
+			services.AddTransient<MainWindow>();
 		}
 
 		private void Application_Startup(object sender, StartupEventArgs e)
 		{
-			string path = Path.Combine(defaultDirectory, defaultFileName);
+			logger.LogDebug("startup started");
 
-			IMainWindowViewModel viewModel = new MainWindowViewModel(path);
+			host.Start();
 
-			MainWindow = new MainWindow(viewModel);
+			IHostApplicationLifetime appLifetime = host.Services.GetRequiredService<IHostApplicationLifetime>();
+
+			IFileLoggerSink sink = host.Services.GetRequiredService<IFileLoggerSink>();
+
+			sink.StartSink();
+
+			using (host.Services.CreateScope())
+			{
+				MainWindow = host.Services.GetRequiredService<MainWindow>();	
+			}
+
+			appLifetime.ApplicationStopping.Register(() =>
+			{
+				MainWindow?.Close();
+			}, useSynchronizationContext: true);
 
 			MainWindow.Show();
+
+			logger.LogInformation("started");
 		}
 
 		private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
 		{
 			if (e.Exception is Exception ex)
 			{
-				LogStatic.Exception(ex, includeStackTrace: true);
+				logger.LogError(ex, "unhandled exception ({ExceptionType})", ex.GetType().FullName);
 			}
 			else
 			{
-				string message = "a DispatcherUnhandledException happened, but was empty";
+				const string nullExceptionMessage = "dispatcher unhandled exception: inner exception was null";
 
-				LogStatic.Message(message);
+				logger.LogCritical(nullExceptionMessage);
+
+				Console.Error.WriteLine(nullExceptionMessage);
 			}
 		}
 	}
