55f76f553a5d7d092c107fa5f12e01f8fb8b4021
Kingloo <k1ngloo@hotmail.com>
2025-06-12 21:17:29 +0200

    Sort feeds by Status
    Adds explicit integers to FeedStatus.

--- RdrLib/Model/FeedStatus.cs
+++ RdrLib/Model/FeedStatus.cs
@@ -1,20 +1,20 @@
 namespace RdrLib.Model
 {
-	public enum FeedStatus
+	public enum FeedStatus : int
 	{
-		None,
-		Ok,
-		Updating,
-		DoesNotExist,
-		Forbidden,
-		ParseFailed,
-		MovedCannotFollow,
-		Timeout,
-		Dns,
-		RateLimited,
-		InternetError,
-		Broken,
-		CertificateRevocationCheckFailed,
-		Other
+		Ok = 0,
+		Updating = 1,
+		DoesNotExist = 2,
+		Forbidden = 3,
+		ParseFailed = 4,
+		MovedCannotFollow = 5,
+		Timeout = 6,
+		Dns = 7,
+		RateLimited = 8,
+		InternetError = 9,
+		Broken = 10,
+		CertificateRevocationCheckFailed = 11,
+		Other = 12,
+		None = 13
 	}
 }
